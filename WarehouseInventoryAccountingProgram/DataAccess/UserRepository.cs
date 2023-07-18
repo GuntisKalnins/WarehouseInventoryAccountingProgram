d4fb4724d027e24fea6da0b47e7fd3e0d748ff7d
GuntisKalnins <guntis.kalnins92@gmail.com>
2023-07-18 17:56:12 +0300

    Created login form

---
+++ WarehouseInventoryAccountingProgram/DataAccess/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace WarehouseInventoryAccountingProgram.DataAccess
+{
+    using System.Configuration;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    /// Class for accessing user data in the database.
+    /// </summary>
+    public class UserRepository
+    {
+        private string connectionString;
+
+        /// <summary>
+        /// Initializes a new instance of the UserRepository class.
+        /// </summary>
+        public UserRepository()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Validates the user's login information against the database.
+        /// </summary>
+        /// <param name="username">The username entered by the user.</param>
+        /// <param name="password">The password entered by the user.</param>
+        /// <returns>True if the user is valid, false otherwise.</returns>
+        public bool ValidateUser(string username, string password)
+        {
+            bool isValidUser = false;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                int count = (int)command.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    isValidUser = true;
+                }
+
+                connection.Close();
+            }
+
+            return isValidUser;
+        }
+    }
+}
---
+++ WarehouseInventoryAccountingProgram/LoginForm.cs
@@ -0,0 +1,55 @@
+﻿namespace WarehouseInventoryAccountingProgram
+{
+    using System;
+    using System.Windows.Forms;
+    using WarehouseInventoryAccountingProgram.DataAccess;
+
+    /// <summary>
+    /// Login form of the Warehouse Inventory Accounting program.
+    /// </summary>
+    public partial class LoginForm : Form
+    {
+        private UserRepository userRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the LoginForm class.
+        /// </summary>
+        public LoginForm()
+        {
+            InitializeComponent();
+            userRepository = new UserRepository();
+        }
+
+        /// <summary>
+        /// Handles the button click event for the login button.
+        /// Validates the user's login information against the database.
+        /// </summary>
+        /// <param name="sender">The object that triggered the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter a username and password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool isValidUser = userRepository.ValidateUser(username, password);
+
+            if (isValidUser)
+            {
+                MessageBox.Show("Login successful!");
+                DashboardForm dashboardForm = new DashboardForm();
+                dashboardForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
