4df2ee114b0c0e7c328c13575600aad50f72bcff
GuntisKalnins <guntis.kalnins92@gmail.com>
2023-07-19 15:53:02 +0300

    Added sales logic

--- WarehouseInventoryAccountingProgram/Forms/SalesForm.Designer.cs
+++ WarehouseInventoryAccountingProgram/Forms/SalesForm.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.btnSell = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 82);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(775, 336);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(12, 34);
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(120, 20);
+            this.numQuantity.TabIndex = 3;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(9, 11);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(150, 34);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(217, 20);
+            this.txtCustomerName.TabIndex = 5;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(150, 11);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(82, 13);
+            this.lblCustomerName.TabIndex = 6;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // btnSell
+            // 
+            this.btnSell.Location = new System.Drawing.Point(691, 30);
+            this.btnSell.Name = "btnSell";
+            this.btnSell.Size = new System.Drawing.Size(75, 23);
+            this.btnSell.TabIndex = 7;
+            this.btnSell.Text = "Sell";
+            this.btnSell.UseVisualStyleBackColor = true;
+            this.btnSell.Click += new System.EventHandler(this.btnSell_Click);
+            // 
+            // SalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSell);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.dgvProducts);
+            this.Name = "SalesForm";
             this.Text = "SalesForm";
+            this.Load += new System.EventHandler(this.SalesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Button btnSell;
     }
 }
---
+++ WarehouseInventoryAccountingProgram/Models/Sale.cs
@@ -0,0 +1,40 @@
+﻿namespace WarehouseInventoryAccountingProgram.Models
+{
+    using System;
+
+    /// <summary>
+    /// Sale transaction.
+    /// </summary>
+    public class Sale
+    {
+        /// <summary>
+        /// Gets or sets the sale ID.
+        /// </summary>
+        public int SaleID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the product ID associated with the sale.
+        /// </summary>
+        public int ProductID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sale date.
+        /// </summary>
+        public DateTime SaleDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the customer associated with the sale.
+        /// </summary>
+        public string CustomerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity sold.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total amount of the sale.
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+    }
+}
