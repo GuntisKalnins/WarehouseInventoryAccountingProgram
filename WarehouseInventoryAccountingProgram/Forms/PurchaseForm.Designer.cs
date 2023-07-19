043020e7ab75a535922e8fda9c5342556456386b
GuntisKalnins <guntis.kalnins92@gmail.com>
2023-07-19 16:54:16 +0300

    PurchaseForm logic added

--- WarehouseInventoryAccountingProgram/Forms/PurchaseForm.Designer.cs
+++ WarehouseInventoryAccountingProgram/Forms/PurchaseForm.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.cmbProduct = new System.Windows.Forms.ComboBox();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.numCost = new System.Windows.Forms.NumericUpDown();
+            this.lblSupplier = new System.Windows.Forms.Label();
+            this.cmbSupplier = new System.Windows.Forms.ComboBox();
+            this.btnCreateOrder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 66);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(776, 372);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.FormattingEnabled = true;
+            this.cmbProduct.Location = new System.Drawing.Point(12, 39);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(173, 21);
+            this.cmbProduct.TabIndex = 1;
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(370, 39);
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(120, 20);
+            this.numQuantity.TabIndex = 2;
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(13, 13);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(44, 13);
+            this.lblProduct.TabIndex = 3;
+            this.lblProduct.Text = "Product";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(367, 13);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Location = new System.Drawing.Point(493, 13);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(28, 13);
+            this.lblCost.TabIndex = 8;
+            this.lblCost.Text = "Cost";
+            // 
+            // numCost
+            // 
+            this.numCost.DecimalPlaces = 2;
+            this.numCost.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numCost.Location = new System.Drawing.Point(496, 39);
+            this.numCost.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numCost.Name = "numCost";
+            this.numCost.Size = new System.Drawing.Size(120, 20);
+            this.numCost.TabIndex = 7;
+            this.numCost.ThousandsSeparator = true;
+            // 
+            // lblSupplier
+            // 
+            this.lblSupplier.AutoSize = true;
+            this.lblSupplier.Location = new System.Drawing.Point(192, 13);
+            this.lblSupplier.Name = "lblSupplier";
+            this.lblSupplier.Size = new System.Drawing.Size(45, 13);
+            this.lblSupplier.TabIndex = 10;
+            this.lblSupplier.Text = "Supplier";
+            // 
+            // cmbSupplier
+            // 
+            this.cmbSupplier.FormattingEnabled = true;
+            this.cmbSupplier.Location = new System.Drawing.Point(191, 39);
+            this.cmbSupplier.Name = "cmbSupplier";
+            this.cmbSupplier.Size = new System.Drawing.Size(173, 21);
+            this.cmbSupplier.TabIndex = 9;
+            // 
+            // btnCreateOrder
+            // 
+            this.btnCreateOrder.Location = new System.Drawing.Point(704, 36);
+            this.btnCreateOrder.Name = "btnCreateOrder";
+            this.btnCreateOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateOrder.TabIndex = 11;
+            this.btnCreateOrder.Text = "Create Order";
+            this.btnCreateOrder.UseVisualStyleBackColor = true;
+            this.btnCreateOrder.Click += new System.EventHandler(this.btnCreateOrder_Click);
+            // 
+            // PurchaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateOrder);
+            this.Controls.Add(this.lblSupplier);
+            this.Controls.Add(this.cmbSupplier);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.numCost);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.cmbProduct);
+            this.Controls.Add(this.dgvProducts);
+            this.Name = "PurchaseForm";
             this.Text = "PurchaseForm";
+            this.Load += new System.EventHandler(this.PurchaseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.ComboBox cmbProduct;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.NumericUpDown numCost;
+        private System.Windows.Forms.Label lblSupplier;
+        private System.Windows.Forms.ComboBox cmbSupplier;
+        private System.Windows.Forms.Button btnCreateOrder;
     }
 }
